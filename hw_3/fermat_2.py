ece493153c34fcfb008bfd0dbc2916052d826857
Samir Silbak <silbak04@gmail.com>
2012-10-09 16:34:37 -0400

    [py] fixed up the code

--- hw_3/fermat_2.py
+++ hw_3/fermat_2.py
@@ -4,7 +4,7 @@
 import gmpy2
 import math
 
-def safe_prime(exponent, count):
+def safe_prime(exponent):
     for k in range(0,10000000):
         p = 10**exponent + k 
 
@@ -14,15 +14,13 @@ def safe_prime(exponent, count):
         if (a == 1): 
             # since p is prime, check to see if
             # p is a safe prime
-            safe_p = 0.5*(p-2)
+            safe_p = gmpy2.c_div(gmpy2.sub(p,1),2)
 
             if (gmpy2.is_prime(int(safe_p))): 
-                count += 1
-                if count == 1: 
-                    print "k = %i \nsafe prime = %i\n" % (k, safe_p)
+                print "k = %i \nsafe prime = %i\n" % (k, safe_p)
                 return
 
-def twin_prime(exponent, count):
+def twin_prime(exponent):
     for k in range(0,10000000):
         p = 10**exponent + k 
 
@@ -35,36 +33,33 @@ def twin_prime(exponent, count):
             twin_p = p + 2
 
             if (gmpy2.is_prime(twin_p)): 
-                count += 1
-                if count == 1: 
-                    print "k = %i \ntwin prime = %i\n" % (k, twin_p)
+                print "k = %i \ntwin prime = %i\n" % (k, twin_p)
                 return
 
-def probable_prime(exponent, count):
+def probable_prime(exponent):
     for k in range(0,10000000):
         p = 10**exponent + k 
         a = gmpy2.powmod(2, p-1, p)
 
         if (a == 1): 
-            count += 1
-            if count == 1: 
-                print "k = %i \nprobable prime = %i\n" % (k, p)
+            print "k = %i \nprobable prime = %i\n" % (k, p)
             return
 
-def is_prime(r, count):
+def is_prime(r):
     for k in range(0,10000000):
-        p = r + k
+        p = gmpy2.add(r, k)
         a = gmpy2.powmod(2, p-1, p)
 
         if (a == 1): 
-            if (gmpy2.is_prime(k)): 
-                count += 1
-                if count == 1: 
-                    print "k = %i \nprime = %i\n" % (k, p)
-                return
+            print "k = %i \nprime = %i\n" % (k, p)
+            return
 
-safe_prime(30,0)
-twin_prime(30,0)
+print "-"*80
+safe_prime(30)
+print "-"*80
+twin_prime(30)
 
-probable_prime(299,0)
-is_prime(4757465356727831999004128791705437327848295392355362834943053456110093459345837361621774882384242053,0)
+print "-"*80
+probable_prime(299)
+print "-"*80
+is_prime(4757465356727831999004128791705437327848295392355362834943053456110093459345837361621774882384242053)

d63e3729b05955f70105421203f45839287b042a
Samir Silbak <silbak04@gmail.com>
2012-10-09 01:42:07 -0400

    [py] changed some variable names

--- hw_3/fermat_2.py
+++ hw_3/fermat_2.py
@@ -3,27 +3,27 @@ import gmpy2
 def safe_prime(exponent, count):
     for k in range(0,10000000):
         p = 10**exponent + k 
-        safe_prime = 0.5*(p-1)
+        safe_p = 0.5*(p-1)
         a = gmpy2.powmod(2, p-1, p)
 
         if (a == 1): 
-            if (gmpy2.is_prime(int(safe_prime))): 
+            if (gmpy2.is_prime(int(safe_p))): 
                 count += 1
                 if count == 1: 
-                    print "k = %i \nsafe prime = %i\n" % (k, safe_prime)
+                    print "k = %i \nsafe prime = %i\n" % (k, safe_p)
                 return
 
 def twin_prime(exponent, count):
     for k in range(0,10000000):
         p = 10**exponent + k 
-        twin_prime = p + 2
+        twin_p = p + 2
         a = gmpy2.powmod(2, p-1, p)
 
         if (a == 1): 
-            if (gmpy2.is_prime(twin_prime)): 
+            if (gmpy2.is_prime(twin_p)): 
                 count += 1
                 if count == 1: 
-                    print "k = %i \ntwin prime = %i\n" % (k, twin_prime)
+                    print "k = %i \ntwin prime = %i\n" % (k, twin_p)
                 return
 
 def probable_prime(exponent, count):
@@ -49,7 +49,7 @@ def is_prime(r, count):
                     print "k = %i \nprime = %i\n" % (k, p)
                 return
 
-#safe_prime(30, 0)
+safe_prime(30, 0)
 twin_prime(30, 0)
 probable_prime(299, 0)
 is_prime(4757465356727831999004128791705437327848295392355362834943053456110093459345837361621774882384242053, 0)
